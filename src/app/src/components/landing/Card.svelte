8a2e0ad00e8d13069348c114314a9951528cb097
Mateusz Zając <matzayonc@gmail.com>
2025-01-15 17:18:32 +0100

    navigation from landing

--- src/app/src/components/landing/Card.svelte
+++ src/app/src/components/landing/Card.svelte
@@ -4,15 +4,23 @@
 	const {
 		title,
 		color,
+		onClick,
+		onMouseEnter,
 		children
 	}: {
 		title: string;
 		color: string;
+		onClick: () => void;
+		onMouseEnter: () => void;
 		children: Snippet;
 	} = $props();
 </script>
 
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
 <div
+	onclick={onClick}
+	onmouseenter={onMouseEnter}
 	class="mx-5 my-12 rounded-3xl bg-{color}-100 p-6 shadow-lg ring-0
 	transition-all duration-300
 	hover:-translate-y-1 hover:shadow-xl hover:ring-8
