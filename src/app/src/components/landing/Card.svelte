8a73fce2765560e701c468f27df52391d1cf4cf4
Mateusz Zając <matzayonc@gmail.com>
2025-01-15 17:15:04 +0100

    created landing page

---
+++ src/app/src/components/landing/Card.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	const {
+		title,
+		color,
+		children
+	}: {
+		title: string;
+		color: string;
+		children: Snippet;
+	} = $props();
+</script>
+
+<div
+	class="mx-5 my-12 rounded-3xl bg-{color}-100 p-6 shadow-lg ring-0
+	transition-all duration-300
+	hover:-translate-y-1 hover:shadow-xl hover:ring-8
+	hover:ring-{color}-200"
+>
+	<h2 class="mb-4 text-xl font-bold text-{color}-600">{title}</h2>
+	<p class="text-{color}-500">{@render children()}</p>
+</div>
---
+++ src/app/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Card from '$components/landing/Card.svelte';
+</script>
+
+<div class="grid h-screen grid-cols-3 gap-8 px-12 py-12">
+	<Card title="Nadaj" color="orange">
+		Twórz zlecenia i znajdź najlepszego przewoźnika dla swojej przesyłki.
+	</Card>
+
+	<Card title="Przewoźnik" color="blue">Znajdź zlecenia i przewieź wybrane przesyłki.</Card>
+	<Card title="Śledź" color="violet">Zobacz status swoich przesyłek i kiedy będą u Ciebie.</Card>
+</div>
