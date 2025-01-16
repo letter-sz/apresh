51745365bc7b1e5791c636cb816fb2c6099d2611
Mateusz Zając <matzayonc@gmail.com>
2025-01-16 13:22:42 +0100

    fixed breaking colors

--- src/app/src/components/landing/Card.svelte
+++ src/app/src/components/landing/Card.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import clsx from 'clsx';
 	import type { Snippet } from 'svelte';
 
 	const {
@@ -21,11 +22,31 @@
 <div
 	onclick={onClick}
 	onmouseenter={onMouseEnter}
-	class="mx-5 my-12 rounded-3xl bg-{color}-100 p-6 shadow-lg ring-0
-	transition-all duration-300
-	hover:-translate-y-1 hover:shadow-xl hover:ring-8
-	hover:ring-{color}-200"
+	class={clsx(
+		'mx-5 my-12 rounded-3xl p-6 shadow-lg ring-0 transition-all duration-300 hover:-translate-y-1 hover:shadow-xl hover:ring-8',
+		{
+			'bg-orange-100 hover:ring-orange-200': color == 'orange',
+			'bg-violet-100 hover:ring-violet-200': color == 'violet',
+			'bg-blue-100 hover:ring-blue-200': color == 'blue'
+		}
+	)}
 >
-	<h2 class="mb-4 text-xl font-bold text-{color}-600">{title}</h2>
-	<p class="text-{color}-500">{@render children()}</p>
+	<h2
+		class={clsx(`mb-4 text-xl font-bold`, {
+			'text-orange-600': color == 'orange',
+			'text-violet-600': color == 'violet',
+			'text-blue-600': color == 'blue'
+		})}
+	>
+		{title}
+	</h2>
+	<p
+		class={clsx(`text-${color}-500`, {
+			'text-orange-500': color == 'orange',
+			'text-violet-500': color == 'violet',
+			'text-blue-500': color == 'blue'
+		})}
+	>
+		{@render children()}
+	</p>
 </div>
