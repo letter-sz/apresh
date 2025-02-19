c621e6d25654500d60fab3483429a9418843720a
Mateusz Zając <matzayonc@gmail.com>
2025-02-19 23:02:49 +0100

    used rstest

--- src/contract/tests/flow_test.rs
+++ src/contract/tests/flow_test.rs
@@ -1,9 +1,10 @@
 use candid::{decode_one, encode_one, Principal};
 use candid::{CandidType, Encode};
-use pocket_ic::{PocketIc, PocketIcBuilder, WasmResult};
-use std::fs;
-use engine::models::shipment::{ShipmentInfo, ShipmentLocation, SizeCategory, PrintableShipment};
+use engine::models::shipment::{PrintableShipment, ShipmentInfo, ShipmentLocation, SizeCategory};
 use engine::utils::hash_secret;
+use pocket_ic::{PocketIc, PocketIcBuilder, WasmResult};
+use rstest::{fixture, rstest};
+use std::fs;
 
 const INIT_CYCLES: u128 = 2_000_000_000_000;
 const LEDGER_CANISTER_ID: &str = "mxzaz-hqaaa-aaaar-qaada-cai";
@@ -146,7 +147,13 @@ async fn mint_tokens(
     }
 }
 
-fn setup_test_environment() -> (PocketIc, Principal, Principal) {
+struct TestEnvironment {
+    pic: PocketIc,
+    contract_id: Principal,
+}
+
+#[fixture]
+fn test_env() -> TestEnvironment {
     let pic = PocketIcBuilder::new()
         .with_nns_subnet()
         .with_application_subnet()
@@ -191,24 +198,45 @@ fn setup_test_environment() -> (PocketIc, Principal, Principal) {
     )
     .expect("Failed to approve");
 
-    (pic, contract_id, ledger_id)
+    TestEnvironment { pic, contract_id }
 }
 
-fn query_canister(pic: &PocketIc, contract_id: Principal, method: &str, args: Vec<u8>, principal: Principal) -> Vec<u8> {
+fn query_canister(
+    pic: &PocketIc,
+    contract_id: Principal,
+    method: &str,
+    args: Vec<u8>,
+    principal: Principal,
+) -> Vec<u8> {
     let result = pic
         .query_call(contract_id, principal, method, args)
         .unwrap();
     get_reply_bytes(result)
 }
 
-fn update_canister(pic: &PocketIc, contract_id: Principal, method: &str, args: Vec<u8>, principal: Principal) -> Vec<u8> {
+fn update_canister(
+    pic: &PocketIc,
+    contract_id: Principal,
+    method: &str,
+    args: Vec<u8>,
+    principal: Principal,
+) -> Vec<u8> {
     let result = pic
         .update_call(contract_id, principal, method, args)
         .unwrap();
     get_reply_bytes(result)
 }
 
-fn create_test_shipment(pic: &PocketIc, contract_id: Principal, name: String) -> u64 {
+struct TestEnvironmentWithShipment {
+    test_env: TestEnvironment,
+    shipment_id: u64,
+}
+
+#[fixture]
+fn test_shipment(
+    test_env: TestEnvironment,
+    #[default("Test Package")] name: String,
+) -> TestEnvironmentWithShipment {
     let customer_name = Some("Test Customer".to_string());
     let shipment_name = name;
     let secret = b"test_secret";
@@ -222,30 +250,25 @@ fn create_test_shipment(pic: &PocketIc, contract_id: Principal, name: String) ->
         SizeCategory::Envelope,
     );
 
-    let qr_options = apresh_qr::QrCodeOptions {
-        gradient: false,
-        link: "https://test.com".to_string(),
-        size: 256,
-        transparent: false,
-    };
-
     let result = update_canister(
-        pic,
-        contract_id,
+        &test_env.pic,
+        test_env.contract_id,
         "createShipment",
         Encode!(
             &customer_name,
             &shipment_name,
             &hashed_secret,
-            &qr_options,
             &shipment_info
         )
         .unwrap(),
         TEST_PRINCIPAL,
     );
 
-    let res: Result<(Vec<u8>, u64), String> = decode_one(&result).unwrap();
-    res.unwrap().1
+    let res: Result<u64, String> = decode_one(&result).unwrap();
+    TestEnvironmentWithShipment {
+        test_env,
+        shipment_id: res.unwrap(),
+    }
 }
 
 // Helper function to extract bytes from WasmResult
@@ -256,10 +279,13 @@ fn get_reply_bytes(result: WasmResult) -> Vec<u8> {
     }
 }
 
-#[test]
-fn test_create_shipment() {
-    let (pic, contract_id, ledger_id) = setup_test_environment();
-    let shipment_id = create_test_shipment(&pic, contract_id, "Test Package".to_string());
+#[rstest]
+fn test_create_shipment(test_shipment: TestEnvironmentWithShipment) {
+    let TestEnvironmentWithShipment {
+        test_env,
+        shipment_id,
+    } = test_shipment;
+    let TestEnvironment { pic, contract_id } = test_env;
 
     // Verify shipment was created
     let result = query_canister(
@@ -273,13 +299,21 @@ fn test_create_shipment() {
     let shipment = decode_one::<Option<PrintableShipment>>(&result).unwrap();
     assert!(shipment.is_some());
     let shipment = shipment.unwrap();
-    // assert_eq!(shipment.name, "Test Package");
+    assert_eq!(shipment.name, "Test Package");
 }
 
-#[test]
-fn test_create_shipment_with_zero_funds() {
-    let (pic, contract_id, ledger_id) = setup_test_environment();
-    
+#[rstest]
+#[case(1_u64, 1_u64)]
+#[case(0_u64, 1_u64)]
+#[case(0_u64, 1_000_000_000_u64)]
+#[rstest]
+fn test_create_shipment_with_zero_funds(
+    test_env: TestEnvironment,
+    #[case] value: u64,
+    #[case] price: u64,
+) {
+    let TestEnvironment { pic, contract_id } = test_env;
+
     let customer_name = Some("Poor Customer".to_string());
     let shipment_name = "Unwanted Package".to_string();
     let secret = b"test_secret";
@@ -287,19 +321,12 @@ fn test_create_shipment_with_zero_funds() {
     let expected_shipment_id = 10_u64;
 
     let shipment_info = ShipmentInfo::new(
-        100_u64,
-        10_u64,
+        value,
+        price,
         ShipmentLocation::new("Origin".to_string(), 40.7128, -74.0060),
         ShipmentLocation::new("Destination".to_string(), 34.0522, -118.2437),
         SizeCategory::Envelope,
     );
-
-    let qr_options = apresh_qr::QrCodeOptions {
-        gradient: false,
-        link: "https://test.com".to_string(),
-        size: 256,
-        transparent: false,
-    };
 
     let result = update_canister(
         &pic,
@@ -309,7 +336,6 @@ fn test_create_shipment_with_zero_funds() {
             &customer_name,
             &shipment_name,
             &hashed_secret,
-            &qr_options,
             &shipment_info
         )
         .unwrap(),
@@ -363,9 +389,9 @@ fn test_create_shipment_with_zero_funds() {
     );
 }
 
-#[test]
-fn test_get_transfer_fee() {
-    let (pic, contract_id, _ledger_id) = setup_test_environment();
+#[rstest]
+fn test_get_transfer_fee(test_env: TestEnvironment) {
+    let TestEnvironment { pic, contract_id } = test_env;
     let result = pic
         .query_call(
             contract_id,
