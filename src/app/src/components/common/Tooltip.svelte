be4dbb187fc39d52886c1e828aa97c2950f6446d
tejks <sarmatys.programmer@gmail.com>
2025-02-22 00:27:45 +0100

    feat: Create the tooltip component

--- src/app/src/components/common/Tooltip.svelte
+++ src/app/src/components/common/Tooltip.svelte
@@ -13,7 +13,7 @@
 	{@render children()}
 
 	<div
-		class="absolute left-full top-1/2 ml-2 w-max -translate-y-1/2 rounded-xl bg-white px-2.5 py-1.5 text-xs text-black opacity-0 shadow-lg transition-opacity duration-300 group-hover/icon:opacity-100"
+		class="absolute left-full top-1/2 ml-2 hidden w-max -translate-y-1/2 rounded-xl bg-white px-2.5 py-1.5 text-xs text-black opacity-0 shadow-lg transition-opacity duration-300 group-hover/icon:block group-hover/icon:opacity-100"
 	>
 		{text}
 	</div>
