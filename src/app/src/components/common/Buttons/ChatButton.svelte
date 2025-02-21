8b02ea23c5458e75b1cfd381542cf6991842d895
tejks <sarmatys.programmer@gmail.com>
2025-02-22 00:27:22 +0100

    feat: Create the chat button

---
+++ src/app/src/components/common/Buttons/ChatButton.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { MessagesSquare } from 'lucide-svelte';
+
+	type Props = {
+		onclick: () => void;
+	};
+
+	let { onclick }: Props = $props();
+</script>
+
+<button
+	type="button"
+	class="mx-5 flex cursor-pointer items-center space-x-2 rounded-lg border border-neutral-400 p-1.5 transition-all duration-300 hover:scale-105"
+	{onclick}
+>
+	<MessagesSquare
+		size={20}
+		class="cursor-pointer text-neutral-600 transition-all duration-300 hover:scale-110"
+	/>
+
+	<div class="text-xs text-neutral-600">Chat with the shipper</div>
+</button>
