795928b25683e360352b334925faa5a5ec908934
tejks <sarmatys.programmer@gmail.com>
2025-03-10 01:48:14 +0100

    feat: Create contact form and info

---
+++ src/app/src/components/common/Inputs/Textarea.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import clsx from 'clsx';
+	import InputWrapper from './InputWrapper.svelte';
+
+	interface DecimalInputProps {
+		class?: string;
+		placeholder?: string;
+		value?: string;
+		required?: boolean;
+		name: string;
+		id: string;
+		label: string;
+	}
+
+	let {
+		class: className,
+		placeholder,
+		value = $bindable(),
+		required,
+		name,
+		id,
+		label
+	}: DecimalInputProps = $props();
+</script>
+
+<InputWrapper {label} {id}>
+	<div class="rounded-lg border-2 focus-within:border-orange-400">
+		<textarea
+			class={clsx(
+				className,
+				'w-full rounded-3xl border-0 bg-transparent px-2 py-1.5 text-sm font-normal text-neutral-600 placeholder-primary placeholder:italic placeholder:text-slate-400 focus:outline-none focus:ring-0'
+			)}
+			{name}
+			autocomplete="off"
+			bind:value
+			rows="5"
+			{placeholder}
+			{id}
+			{required}
+		>
+		</textarea>
+	</div>
+</InputWrapper>
